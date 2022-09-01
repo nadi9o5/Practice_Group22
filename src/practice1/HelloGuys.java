430022e17d638ffa1e86d57dc2ff68bdba961827
Veronica <marandici.veronica@gmail.com>
2022-08-31 22:15:35 -0400

    first

--- src/practice1/HelloGuys.java
+++ src/practice1/HelloGuys.java
@@ -2,9 +2,13 @@ package practice1;
 
 public class HelloGuys {
     public static void main(String[] args) {
-
-        System.out.println("first we created hello guys class");
-
-
+        System.out.println("hello");
+        System.out.println("veronica say hi");
     }
+
+
+
+
+
+
 }
